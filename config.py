b00f8ea36859effc96c6d58ab9bf596fab069635
vks <vsvsasas@gmail.com>
2026-03-17 23:12:48 +0530

    update

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 
 
 
-# Using llama3 for extraction/logic and tinyllama for fast summarization
+# Using qwen2.5:0.5b for extraction/logic and tinyllama for fast summarization
 EMBEDDER_MODEL = "nomic-embed-text"
 SUMMARY_MODEL = "qwen2.5:0.5b"
 DB_PATH = "./local_rag_db"
